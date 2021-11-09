f28fc761a6329ccfbbfc8e189578b557f484d7df
Nathan Waltz <nathan.waltz@wsu.edu>
2021-11-08 23:46:28 -0800

    Updated generate_event_counter

--- stats.py
+++ stats.py
@@ -9,13 +9,28 @@ def equal_or_empty(feature, event, element):
 
 def process_event_counts(events: List[object],
                          features: Dict[str, object],
-                         tactics: Set[str]) -> Dict[str, int]:
+                         tactics: Dict[str, object]) -> Dict[str, int]:
     pass
 
 
-def generate_event_counter(key: str):
+def generate_event_counter(event_key: str):
     def local_closure(events: List[object],
                       features: Dict[str, object],
-                      tactics: Set[str]) -> Dict[str, int]:
-        pass
-    pass
+                      tactics: Dict[str, object]) -> Dict[str, int]:
+        counts = dict()
+        for event in events:
+            id = event[event_key]
+            anomalous = False
+            found_tactics = dict(map(lambda x: (x, False), tactics))
+            for key, value in features.items():
+                if id in value:
+                    anomalous = True
+                    for tactic_key, tactic_value in tactics.items():
+                        if not found_tactics[tactic_key] and key in tactic_value:
+                            counts[tactic_key] += 1
+                            found_tactics[tactic_key] = True
+
+            counts['num_anomalous'] += 1 if anomalous else 0
+
+        return counts
+    return local_closure
