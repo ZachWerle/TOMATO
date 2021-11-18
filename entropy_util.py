f5b4a8e5cf740c781eae7b826e684106fcbbf9b5
Nathan Waltz <nathan.waltz@wsu.edu>
2021-11-18 14:55:51 -0800

    Made a small change

--- entropy_util.py
+++ entropy_util.py
@@ -54,7 +54,7 @@ def create_on_host_tactic_matrix(graph: Graph) -> np.matrix:
     """
     probability_matrix = np.zeros((graph.number_of_nodes(), graph.number_of_nodes()))
     for i, j in zip(range(0, 10), range(0, 10)):
-        probability_matrix[i, j] = 1 / NUM_FEATURES
+        probability_matrix[i, j] = 1
     
     return probability_matrix
 
@@ -64,12 +64,12 @@ def create_probability_matrix(graph: Graph) -> np.matrix:
     Creates the probability matrix. This computes the probability of
     attacks occuring on a given host. 
     """
-    # each of these is being treated as having an equal likelihood of occuring
+    # each of these is being treated as having an equal likelihood of occuring,
+    # so we have to do this to lateral movement matrix as well.
     lateral_movement_matrix = create_lateral_movement_matrix(graph) * (1 / NUM_FEATURES)
-
-    discovery_matrix = create_on_host_tactic_matrix(graph)
-    execution_matrix = create_on_host_tactic_matrix(graph)
-    privilege_escalation_matrix = create_on_host_tactic_matrix(graph)
+    discovery_matrix = create_on_host_tactic_matrix(graph) * (1 / NUM_FEATURES)
+    execution_matrix = create_on_host_tactic_matrix(graph) * (1 / NUM_FEATURES)
+    privilege_escalation_matrix = create_on_host_tactic_matrix(graph) * (1 / NUM_FEATURES)
     return np.array([lateral_movement_matrix, discovery_matrix, execution_matrix, privilege_escalation_matrix])
 
 
